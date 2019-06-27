fbdac7cecea1de2bc0187751dfb9c68a550e6130
Hakim Rachidi <hakimrachidi02@gmail.com>
2019-06-27 23:34:10 +0200

    Add nanotube arkit support

--- js/app.js
+++ js/app.js
@@ -5,6 +5,26 @@
     else{
         console.error("Error while getting model name");
     }
+
+    const isARCompatible = (function () {
+        try {
+            var a = document.createElement("a");
+            return a.relList.supports("ar");
+        }
+        catch(err) {
+            if (err instanceof TypeError) {
+                return false;
+            }
+        }
+    }());
+
+    if(isARCompatible) {
+        $("#MoleculeAR").css("display", "inline-block");
+    }
+    
+
+
+
     var graphicDIV = document.getElementById("graphic");
     Graphic(window, THREE, graphicDIV, `../graphics/${matches[1]}.drc`);
 }());
